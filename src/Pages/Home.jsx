d847ad80a9cdb60fd515ff33846208ca7fe390eb
Pratheek Gowda <gowdapratheek91@gmail.com>
2024-10-24 23:30:50 +0530

    video done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,13 +5,7 @@ import Testimonials from "./Testimonials";
 import WhyChooseUs from "./WhyChooseUs";
 import Services from "./Services";
 
-const images = [
-  "/landing-page.jpg",
-  "/it-logo.png",
-  "/fav.png",
-  "/testimonails-img.png",
-  "/testimonails-img.png",
-];
+const images = ["/1.jpg", "/2.jpg", "/3.jpg", "/4.jpg"];
 
 function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -27,28 +21,23 @@ function Home() {
 
   return (
     <>
+      <video
+        className="w-full h-[85vh] object-cover object-center"
+        src="/1.mp4"
+        autoPlay
+        muted
+        onEnded=""
+      />
       <div
         className="relative h-[85vh] w-full bg-cover bg-no-repeat bg-center"
         style={{
           backgroundImage: `url(${images[currentImageIndex]})`,
         }}
       >
-        <div className="absolute inset-0 z-0 bg-black opacity-45"></div>
+        {/*<div className="absolute inset-0 z-0 bg-black opacity-45"></div>*/}
 
-        <div className="relative z-10 flex flex-col justify-center items-center h-full px-4 text-center">
-          <h1 className="text-[2rem] lg:text-[3rem] font-bold text-[#00BCFF]">
-            Professional IT Services
-          </h1>
-          <p className="text-center lg:w-[50%] w-full lg:text-base text-sm text-white">
-            We empower your business with expert Managed IT Services, The IT
-            District offers top-tier Managed IT Services and IT Support,
-            delivered by skilled, certified technicians utilizing cutting-edge
-            technologies and proven processes. We guarantee 24x7 uptime or
-            servers, network devices, infrastructure, and business applications,
-            ensuring seamless service.
-          </p>
-
-          <div className="flex lg:w-1/3 w-full justify-around mt-4">
+        <div className="relative z-10 flex flex-col items-center h-[85vh] px-4 text-center top-[70%]">
+          <div className="flex  lg:w-1/3 w-full justify-around">
             <a
               href="#services"
               className="cursor-pointer border border-[#00BCFF] w-[45%] text-sm lg:text-base py-2 bg-gradient-to-r from-[#00BCFF] to-[#007FFF] rounded-md text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-white"
